da94960f2e1e1b051e9dcb16ba4854560af2334a
Nathalie B <37987956+nathbrow42@users.noreply.github.com>
2021-12-06 21:12:53 -0500

    part 1 completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'fav-photos',
-  templateUrl: './fav-photos.component.html',
-  styleUrls: ['./fav-photos.component.css']
+  selector: "fav-photos",
+  templateUrl: "./fav-photos.component.html",
+  styleUrls: ["./fav-photos.component.css"],
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "Favorite Photos";
+  image1 =
+    "https://www.akc.org/wp-content/uploads/2017/11/Labrador-Retriever-MP-500x486.jpg";
+  image2 =
+    "https://www.readersdigest.ca/wp-content/uploads/2013/03/6-facts-to-know-before-owning-a-puppy.jpg";
+  image3 =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq4YXHU5CzMxbh3ZV4tyNJ2UqcD5wxmObunw&usqp=CAU";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
